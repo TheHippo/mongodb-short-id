0d81c84ba0456480a7d9e229b7d5600015db05f7
Philipp Klose <me@thehippo.de>
2013-04-06 17:16:00 +0200

    accepting and returning mongodb objectids

--- src/mongodb-short-id.js
+++ src/mongodb-short-id.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 function shortToLong(shortID) {
 	shortID = shortID.replace('-', '+').replace('_', '/');
 	var buffer = new Buffer(shortID, 'base64');
@@ -9,10 +11,22 @@ function longToShort(longID) {
 	return buffer.toString('base64').replace('+', '-').replace('/', '_');
 }
 
+function shortToObjectID(shortID) {
+	return new ObjectID(shortToLong(shortID));
+}
+
+function objectIDtoShort(objectID) {
+	return longToShort(objectID.toHexString());
+}
+
 module.exports = {
 	shortToLong:shortToLong,
 	longToShort:longToShort,
 	l2s:longToShort,
-	s2l:shortToLong
+	s2l:shortToLong,
+	shortToObjectID:shortToObjectID,
+	objectIDtoShort:objectIDtoShort,
+	o2s:objectIDtoShort,
+	s2o:shortToObjectID
 };
 
