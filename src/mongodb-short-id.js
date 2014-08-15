5bbfed2ba444f3315448009a591f1e7bea99689a
Derrick Dominic <derrickdominic@gmail.com>
2014-08-15 10:33:23 -0700

    Short ids now replace all + and / characters.
    Using the 'g' flag in the string 'replace' function to replace all instances of + and / characters.

--- src/mongodb-short-id.js
+++ src/mongodb-short-id.js
@@ -1,14 +1,14 @@
 var ObjectID = require('mongodb').ObjectID;
 
 function shortToLong(shortID) {
-	shortID = shortID.replace('-', '+').replace('_', '/');
+	shortID = shortID.replace(/-/g, '+').replace(/_/g, '/');
 	var buffer = new Buffer(shortID, 'base64');
 	return buffer.toString('hex');
 }
 
 function longToShort(longID) {
 	var buffer = new Buffer(longID, 'hex');
-	return buffer.toString('base64').replace('+', '-').replace('/', '_');
+	return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
 }
 
 function shortToObjectID(shortID) {
